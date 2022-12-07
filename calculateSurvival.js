3da2173d419a172ccf68543ff03166a03de65476
jeremiah-dy <97951461+jeremiah-dy@users.noreply.github.com>
2022-12-07 10:15:28 -1000

    Added charts into the main page (index.html)

--- calculateSurvival.js
+++ calculateSurvival.js
@@ -26,9 +26,11 @@ function calculateSurvival() {
   // Classification threshold 0.4163379 (mean predicted probability)
   if (survival >= 0.4163379) {
     header.textContent = "You survived! :)"
-    message.textContent = "Your probability of survival was " + (survival*100).toFixed(2) + "%. Looks like Lady Luck was smiling upon you as you embarked on your journey. At the very least, you'll have a story for the ages! Although I would look into some therapy sessions to treat any mental trauma this experience might have caused you. We hope you sail with us again soon!"
+    message.textContent = "Your probability of survival was " + (survival*100).toFixed(2) + "%. " +
+      "Looks like Lady Luck was smiling upon you as you embarked on your journey. At the very least, you'll have a story for the ages! We recommend you look into some therapy sessions to treat any mental trauma this experience might have caused you. Have a wonderful rest of your trip, and we hope you sail with us again soon!"
   } else {
     header.textContent = "Oh no! You died! :(";
-    message.textContent = "Your probability of survival was " + (survival*100).toFixed(2) + "%. It appears the odds were not in your favor. Oh, wait! There's a boat over there! If you swim fast enough you might make it! Oops, looks like it just Charon. Hopping from one boat to the next, huh? You know, the lower your passenger class the lower your cabin on deck. That's a lot of stairs! Welp, toodaloo!"
+    message.textContent = "Your probability of survival was " + (survival*100).toFixed(2) + "%. " +
+      "It appears the odds were not in your favor. Maybe if you had been more ruthless, or been more pitiable, your fellow passengers might have let you on one of the earlier lifeboats. Unfortunately, you'll never know, as the icy waters of the Atlantic ocean has claimed your body for the fishes. Sleep well! P.S. You know, the lower your passenger class the lower your cabin on deck. That's a lot of stairs! Toodaloo!"
   }
 };
